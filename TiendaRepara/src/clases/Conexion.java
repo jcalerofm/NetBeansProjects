14256b261219420389775baf30016716e277e65c
Jorge Calero <jorge.calero@gmail.com>
2023-02-10 09:33:38 +0100

    metodo registrarCliente

--- TiendaRepara/src/clases/Conexion.java
+++ TiendaRepara/src/clases/Conexion.java
@@ -1,7 +1,9 @@
 package clases;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class Conexion {
@@ -15,5 +17,26 @@ public class Conexion {
             System.err.println("Error en la conexión local " + e);
         }
         return (null);
+    }
+
+    //Crear un metodo para Registrar cliente en la base de datos con los datos nombre, apellido1, apellido2, dni, direccion, telefono, email, fecha_registro
+    public static void registrarCliente(String nombre, String apellido1, String apellido2, String dni, String direccion, String telefono, String email, Date fecha_registro){
+        try {
+            Connection cn = Conexion.conectar();
+            PreparedStatement pst = cn.prepareStatement("insert into clientes values(?,?,?,?,?,?,?,?)");
+            pst.setString(1, nombre);
+            pst.setString(2, apellido1);
+            pst.setString(3, apellido2);
+            pst.setString(4, dni);
+            pst.setString(5, direccion);
+            pst.setString(6, telefono);
+            pst.setString(7, email);
+            pst.setDate(8, fecha_registro);
+            pst.executeUpdate();
+            System.out.println("Cliente registrado con exito");
+            cn.close();
+        } catch (SQLException e) {
+            System.err.println("Error al registrar cliente " + e);
+        }
     }
 }
